6a3feb7e06646fc2c3fd959870e130b1ab1e8e2b
Dominik Kroliczek <dominik@kroliczek.com>
2019-06-01 16:46:55 +0200

    fix2 xd

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export * from './src/account';
 export * from './src/car';
-export * from './src//interfaces';
+export * from './src/common';
 export * from './src/register';
 export * from './src/trophy';
