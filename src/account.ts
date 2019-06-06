97239b5f0f25a6345810b52f8bf51193425fd140
Dominik Kroliczek <dominik@kroliczek.com>
2019-06-06 21:45:44 +0200

    GetReponsePhoto

--- src/account.ts
+++ src/account.ts
@@ -1,9 +1,9 @@
-import { GetResponseCar, GetResponseTrophy } from './common';
+import { GetResponseCar, GetResponseTrophy, GetResponsePhoto } from './common';
 
 export interface GetMyAccountResponse {
   login: string;
   nick: string;
-  photo: string;
+  photo: GetResponsePhoto;
   isActive: boolean;
   trophies: GetResponseTrophy[];
   cars: GetResponseCar[];
